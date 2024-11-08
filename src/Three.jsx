ea9fd38a85c27d444c38fb0ecc6114ebe40c4634
Luis Vicente Reyes <102662773+luisdlpr@users.noreply.github.com>
2024-11-08 19:01:07 +1100

    compress model

--- src/Three.jsx
+++ src/Three.jsx
@@ -2,10 +2,21 @@ import { Canvas } from "@react-three/fiber";
 import { useLoader } from "@react-three/fiber";
 import { Environment, OrbitControls } from "@react-three/drei";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import { Suspense } from "react";
+import { Html, useProgress } from "@react-three/drei";
+
+function Loader() {
+  const { progress } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
+}
 
 const Scene = () => {
-  const gltf = useLoader(GLTFLoader, "model.gltf");
+  const gltf = useLoader(GLTFLoader, "model_compressed.gltf", (loader) => {
+    const dracoLoader = new DRACOLoader();
+    dracoLoader.setDecoderPath("gltfLoader/gltf/");
+    loader.setDRACOLoader(dracoLoader);
+  });
 
   return <primitive object={gltf.scene} scale={3} />;
 };
@@ -14,7 +25,7 @@ export default function MyThree() {
   return (
     <div style={{ width: "300px", height: "300px" }}>
       <Canvas>
-        <Suspense fallback={null}>
+        <Suspense fallback={<Loader />}>
           <Scene />
           <OrbitControls />
           <Environment preset="forest" background />
